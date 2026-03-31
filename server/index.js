27e668749c6a6ac087cc3e3e0a85520842f7e00c
VerbalDye <dhunts258@gmail.com>
2026-03-31 12:47:17 -0400

    initial setup

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+// const sequelize = require('./config/connection');
+const path = require('path');
+// const routes = require('./controllers');
+// const cookieParser = require('cookie-parser');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false, limit: '10mb' }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+
+// sequelize.authenticate().then(() => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}`);
+        });
+// });
