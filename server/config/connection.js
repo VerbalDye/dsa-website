e76033eb7b4464bb747d5b91b2a9ee0db53181ef
VerbalDye <dhunts258@gmail.com>
2026-04-03 14:08:34 -0400

    fix sql connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 console.log(process.env.DB_NAME)
 console.log(process.env.DB_USER)
+console.log(process.env.DB_PW)
 
 let sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: '127.0.0.1',
