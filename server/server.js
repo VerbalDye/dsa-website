63e0458aca17905f79d038432596f5a2ac7e8caf
VerbalDye <dhunts258@gmail.com>
2026-04-03 13:49:44 -0400

    add basic calendar

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false, limit: '10mb' }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.static(path.join(__dirname, 'client/dist')));
 app.use(routes);
-
+console.log("attempting")
 sequelize.authenticate().then(() => {
         app.listen(PORT, () => {
             console.log(`API server running on port ${PORT}`);
