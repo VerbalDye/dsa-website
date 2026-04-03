c233f4f6ddafb8363f63f6568439c64be4faaadb
VerbalDye <dhunts258@gmail.com>
2026-04-03 14:14:35 -0400

    fix sql connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const sequelize = require('./config/connection');
+// const sequelize = require('./config/connection');
 const path = require('path');
 const routes = require('./controllers');
 const cookieParser = require('cookie-parser');
