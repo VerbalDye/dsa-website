e0a4048df4f266cbeb3bf05b8fa53644ae33659e
VerbalDye <dhunts258@gmail.com>
2026-04-03 14:21:15 -0400

    fix sql connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -8,7 +8,7 @@ let sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.
     host: '127.0.0.1',
     dialect: 'mysql',
     port: 3306,
-    logging: false
+    logging: true
 });
 
 console.log(sequelize);
